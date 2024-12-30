0a1b01d809e9d0c0f6452d25aa8e7f8182869143
Adrian Rosu <adrianrosu5400@gmail.com>
2024-12-31 00:57:24 +0200

    Add files via upload

--- Gravity_celestial_bodies/Polaris_system.py
+++ Gravity_celestial_bodies/Polaris_system.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 solar_mass = 2e30 #kg
 
-PolarisAa = CelestialBody(mass = 5.13 * solar_mass, position_vector=[0, 0, 0], velocity_vector=[0, 14, 0], name = "Polaris Aa")
-PolarisAb = CelestialBody(mass = 1.31 * solar_mass, position_vector=[75000, 0, 0], velocity_vector=[0, -81.5, 0], name = "Polaris Ab")
-PolarisB  = CelestialBody(mass = 1.39 * solar_mass, position_vector=[0, 190000, 10000], velocity_vector=[55, 12, -2.5], name = "Polaris B")
+PolarisAa = CelestialBody(mass = 5.13 * solar_mass, position_vector=[0, 0, 0], velocity_vector=[0, 0, 0], name = "Polaris Aa")
+PolarisAb = CelestialBody(mass = 1.31 * solar_mass, position_vector=[-100000, 0, -5000], velocity_vector=[0, 82, 1], name = "Polaris Ab")
+PolarisB  = CelestialBody(mass = 1.39 * solar_mass, position_vector=[0, 200000, 150000], velocity_vector=[45, 0, -10], name = "Polaris B")
 
 stars = [PolarisAa, PolarisAb, PolarisB]
 
@@ -23,6 +23,7 @@ for _ in range(60000):
 sim.plot_data_2D()
 
 """save position data in a csv file (million kilometers unit)"""
+
 Aa_data = np.round(PolarisAa.position_data[0::100] / 1000, 3)
 Ab_data = np.round(PolarisAb.position_data[0::100] / 1000, 3)
 B_data = np.round(PolarisB.position_data[0::100] / 1000, 3)
@@ -35,7 +36,7 @@ print(all_data[-1])
 
 filepath = "Gravity_celestial_bodies/Polaris_data.csv"
 
-data_names = ['1x','1y','1z','2x','2y','2z','3x','3y','3z']
+data_names = ['PolarisAa_x','PolarisAa_y','PolarisAa_z','PolarisAb_x','PolarisAb_y','PolarisAb_z','PolarisB_x','PolarisB_y','PolarisB_z']
 
 df = pd.DataFrame(all_data, columns=data_names, index=frame_data)
 
